746ae3f6726ec560bae53c97c0a79284457816b9
Younus Ali Akash <younusaliakash8@gmail.com>
2021-12-17 17:47:27 +0600

    Project init

---
+++ src/Components/Stepper/Stepper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Stepper';
+
+const Stepper = () => {
+    return (
+        <div>
+            <h1>Stepper</h1>
+        </div>
+    );
+};
+
+export default Stepper;
